b1a708ab3dbdbcdaad4bd8e76df0436949db0dfc
Jeuel Gutierrez <Jgutierrez220601@yahoo.co.uk>
2020-09-04 17:08:14 +0100

    Add files via upload

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Header from '../Common/Header';
+import {image} from '../assets/img/header-bg.jpg';
+
+//Re-usable components
+import Services from '../Common/Services';//two '.' because Services is two directories up (from Pages to Common)
+import Portfolio from '../Common/Portfolio';
+
+class Home extends Component {
+
+    render(){
+        return(
+            
+        <div>
+            <Header
+                 title="Welcome To Our Studio!"
+                 subtitle="IT'S NICE TO MEET YOU"
+                 buttonText="Tell me more"
+                 link="/services"
+                 showButton={true}
+                 image={image}
+
+            />
+
+            <Services />
+            <Portfolio />
+        </div>
+
+        )
+    }
+}
+
+export default Home;
