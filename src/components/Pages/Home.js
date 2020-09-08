5d7c79c7ea5f52455585bcab04028a7773865be1
Jeuel Gutierrez <Jgutierrez220601@yahoo.co.uk>
2020-09-08 10:24:35 +0100

    Add files via upload

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -6,6 +6,7 @@ import {image} from '../assets/img/header-bg.jpg';
 import Services from '../Common/Services';//two '.' because Services is two directories up (from Pages to Common)
 import Portfolio from '../Common/Portfolio';
 import Timeline from '../Common/Timeline';
+import Team from '../Common/Team'
 
 class Home extends Component {
 
